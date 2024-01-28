0dfaba4417cf6a4f58e0b4bc10a88ffe11b73700
daniellesarau <123309317+daniellesarau@users.noreply.github.com>
2024-01-28 16:35:24 +0200

    boardSchema

--- src/models/board/board.js
+++ src/models/board/board.js
@@ -1,59 +1,61 @@
 const { Schema, model } = require("mongoose");
 
 const handleMongooseError = (error, data, next) => {
-  error.status = 400;
-  next();
+ error.status = 400;
+ next();
 };
 
 const boardSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: [true, "Set the board's title"],
-    },
-    icon: { type: String, required: true },
-    background: {
-      min: { type: String },
-      desktop: { type: String },
-      tablet: { type: String },
-      mobile: { type: String },
-    },
-    filter: {
-      type: String,
-      enum: ["without", "low", "medium", "high"],
-      default: "without",
-    },
-    columns: [
-      {
-        title: { type: String, required: true },
-        owner: {
-          type: Schema.Types.ObjectId,
-        },
-        cards: [
-          {
-            title: { type: String, required: true },
-            text: { type: String, required: true },
-            priority: {
-              type: String,
-              enum: ["without", "low", "medium", "high"],
-              default: "without",
-            },
-            deadline: { type: String, required: true },
-            owner: {
-              type: Schema.Types.ObjectId,
-              required: true,
-            },
-          },
-        ],
-      },
-    ],
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-      required: true,
-    },
+ {
+  title: {
+   type: String,
+   required: [true, "Set the board's title"],
   },
-  { versionKey: false, timestamps: true }
+  icon: { type: String, required: true },
+  background: { type: String },
+  // background: {
+  //  min: { type: String },
+  //  desktop: { type: String },
+  //  tablet: { type: String },
+  //  mobile: { type: String },
+  // },
+
+  filter: {
+   type: String,
+   enum: ["without", "low", "medium", "high"],
+   default: "without",
+  },
+  columns: [
+   {
+    title: { type: String, required: true },
+    owner: {
+     type: Schema.Types.ObjectId,
+    },
+    cards: [
+     {
+      title: { type: String, required: true },
+      text: { type: String, required: true },
+      priority: {
+       type: String,
+       enum: ["without", "low", "medium", "high"],
+       default: "without",
+      },
+      deadline: { type: String, required: true },
+      owner: {
+       type: Schema.Types.ObjectId,
+       required: true,
+      },
+     },
+    ],
+   },
+  ],
+  owner: {
+   type: Schema.Types.ObjectId,
+   ref: "user",
+   required: true,
+  },
+ },
+ { versionKey: false, timestamps: true }
 );
 
 boardSchema.post("save", handleMongooseError);
@@ -61,5 +63,5 @@ boardSchema.post("save", handleMongooseError);
 const Board = model("board", boardSchema);
 
 module.exports = {
-  Board,
+ Board,
 };
